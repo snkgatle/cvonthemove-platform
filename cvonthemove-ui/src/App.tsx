29b8eb1d79a38bfd6d4f8c2bfa6a2d212688cef0
Stephans Kgatle <stephans.kgatle@gmail.com>
2025-12-24 18:30:43 +0200

    feat: Establish initial React UI routing and implement GitHub Actions for Cloud Run API and App Engine UI deployment.

--- cvonthemove-ui/src/App.tsx
+++ cvonthemove-ui/src/App.tsx
@@ -1,10 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './features/landing/LandingPage';
-import CreateCVPage from './features/cv-builder/pages/CreateCVPage';
-import EditCVPage from './features/cv-builder/pages/EditCVPage';
-import DownloadCVPage from './features/cv-builder/pages/DownloadCVPage';
 import LoginPage from './features/auth/pages/LoginPage';
 import './App.css';
+import { CreateCVPage, EditCVPage } from './features/cv-builder/components/Pages';
 
 function App() {
   return (
@@ -14,7 +12,7 @@ function App() {
         <Route path="/create" element={<CreateCVPage />} />
         <Route path="/edit" element={<EditCVPage />} />
         <Route path="/edit/:id" element={<EditCVPage />} />
-        <Route path="/download" element={<DownloadCVPage />} />
+        <Route path="/download" element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
